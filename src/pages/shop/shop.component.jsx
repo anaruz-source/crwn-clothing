da880e2256423954ba61678c6396076d8872b434
root <abdelhaq.fste@gmail.com>
2021-02-12 13:34:03 +0100

    added collection-preview, collection-item components, updated app with those components

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import '../../components/collection-preview/collection-preview.component'
+import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+
+import SHOP_DATA from './shop.data'
+
+class ShopPage extends React.Component {
+
+    constructor(props){
+
+        super(props)
+
+        this.state = SHOP_DATA
+    }
+
+    render() {
+
+        const collections = this.state
+
+        return (<div>
+
+            { collections.map( ({id, ...otherProps}) => (
+
+                <CollectionPreview key= {id} {...otherProps } />
+            ))}
+        </div>)
+    }
+}
+
+export default ShopPage
