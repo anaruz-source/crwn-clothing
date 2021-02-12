ccb1cef671a7e98632581eccb4ac788ac19417f6
root <abdelhaq.fste@gmail.com>
2021-02-12 21:35:54 +0100

    added Header component with home, shop, contact links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
@@ -8,6 +9,7 @@ import { Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <div>
+      <Header className='header'/>
       <Switch>
         <Route exact path='/' component= {HomePage} />
         <Route  path='/shop' component= {ShopPage} />
